9ba18fa74022ccc12d120d6967fa5463379711e5
chamRun <chrm@aut.ac.ir>
2022-01-06 13:13:17 +0330

    calculate_new_capacity Completed

--- module_3/calculate_new_capacity.v
+++ module_3/calculate_new_capacity.v
@@ -19,11 +19,22 @@
 -----------------------------------------------------------*/
 `timescale 1 ns/1 ns
 module calculate_new_capacity(
- park_location,
- parking_capacity,
- new_capacity);
+    park_location,
+    parking_capacity,
+    new_capacity);
+
 input [7:0] park_location;
 input [7:0] parking_capacity;
 output [7:0] new_capacity;
- // write your code here, please.
+
+or(new_capacity[0], park_location[0], parking_capacity[0]);
+or(new_capacity[1], park_location[1], parking_capacity[1]);
+or(new_capacity[2], park_location[2], parking_capacity[2]);
+or(new_capacity[3], park_location[3], parking_capacity[3]);
+or(new_capacity[4], park_location[4], parking_capacity[4]);
+or(new_capacity[5], park_location[5], parking_capacity[5]);
+or(new_capacity[6], park_location[6], parking_capacity[6]);
+or(new_capacity[7], park_location[7], parking_capacity[7]);
+
+
 endmodule
