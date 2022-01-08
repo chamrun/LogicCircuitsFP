907e8860a864cd45e2c17479a228255d25b7b7fb
chamRun <chrm@aut.ac.ir>
2022-01-08 21:51:00 +0330

    uploading module 6&7 templates

---
+++ module_7/controller.v
@@ -0,0 +1,34 @@
+/*--  *******************************************************
+--  Computer Architecture Course, Laboratory Sources 
+--  Amirkabir University of Technology (Tehran Polytechnic)
+--  Department of Computer Engineering (CE-AUT)
+--  https://ce[dot]aut[dot]ac[dot]ir
+--  *******************************************************
+--  All Rights reserved (C) 2021-2022
+--  *******************************************************
+--  Student ID  : 
+--  Student Name: 
+--  Student Mail: 
+--  *******************************************************
+--  Additional Comments:
+--
+--*/
+
+/*-----------------------------------------------------------
+---  Module Name: controller
+-----------------------------------------------------------*/
+`timescale 1 ns/1 ns
+module controller(
+input [2:0] system_token,
+input request,
+input [7:0] TimeData,
+input reset,
+input clock,
+input confirm,
+input [2:0] user_token,
+output [7:0] data_Q,
+output [7:0] data_P
+);
+
+ // write your code here, please.
+endmodule
